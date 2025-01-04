7e06e91eb03708dc62b92972a662b5675c9a4a3b
Leon Di Stefano <l@sdf.org>
2025-01-04 19:32:22 +1100

    Add roxygen directives to function

--- R/rank_gap.R
+++ R/rank_gap.R
@@ -1,3 +1,12 @@
+#' Compute rank-gap statistics from input scores
+#' 
+#' @export
+#' 
+#' @param ... ≥2 vectors of input scores. These should all have the same length. (Missing values?)
+#' @param ties_method Method of breaking ties; passed to `rank()`
+#' @param nonzero_adj Whether to adjust ranks to avoid zero values of rank-gap statistics
+#' 
+#' @return A vector of rank-gap statistics, the same length as the input scores
 rank_gap <- function(..., ties_method = "random", nonzero_adj = TRUE) {
   stats_list <- list(...)
   n_stats    <- length(stats_list)
