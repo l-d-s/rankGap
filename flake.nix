7d6978ace2a5e8886bcc1b48b7cae1e7a45bddd5
Leon Di Stefano <l@sdf.org>
2025-01-23 08:49:00 +1100

    Format using nixfmt

--- flake.nix
+++ flake.nix
@@ -4,77 +4,84 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-    ...
-  } @ inp:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inp:
 
-  let
-    l = nixpkgs.lib // builtins;
-    supportedSystems = ["x86_64-linux" "aarch64-darwin"];
-    forAllSystems = f:
-      l.genAttrs supportedSystems
-      (system: f system (import nixpkgs {inherit system;}));
+    let
+      l = nixpkgs.lib // builtins;
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        f: l.genAttrs supportedSystems (system: f system (import nixpkgs { inherit system; }));
 
-  in
+    in
 
-  {
-    # enter this python environment by executing `nix shell .`
-    devShell = forAllSystems (
-      system: pkgs: let
-        rPackages = pkgs.rPackages;
-        
-        vscodeRPackages = with rPackages; [
-          languageserver
-          httpgd
-          rlang
-          jsonlite
-          # needed for knitr I think
-          devtools
-        ];
-        additionalRPackages = with rPackages; [
-          ggplot2
-          cowplot
-          colorspace
-          # patchwork
-          forcats
+    {
+      # enter this python environment by executing `nix shell .`
+      devShell = forAllSystems (
+        system: pkgs:
+        let
+          rPackages = pkgs.rPackages;
 
-          # For building the readme
-          eulerr
-          qvalue
+          vscodeRPackages = with rPackages; [
+            languageserver
+            httpgd
+            rlang
+            jsonlite
+            # needed for knitr I think
+            devtools
+          ];
+          additionalRPackages = with rPackages; [
+            ggplot2
+            cowplot
+            colorspace
+            # patchwork
+            forcats
 
-          # For development
-          tidyverse
-        ];
-        rPackageList = vscodeRPackages ++ additionalRPackages;
-        radianWrapper = pkgs.radianWrapper.override {
+            # For building the readme
+            eulerr
+            qvalue
+
+            # For development
+            tidyverse
+          ];
+          rPackageList = vscodeRPackages ++ additionalRPackages;
+          radianWrapper = pkgs.radianWrapper.override {
             packages = rPackageList;
-            };
-        rWrapper = pkgs.rWrapper.override {
-          packages = rPackageList;
-        };
-      in
-        pkgs.mkShell { 
-          shellHook = ''
-            export RADIAN_BIN=${radianWrapper.outPath}/bin/radian
-            export R_BIN=${rWrapper.outPath}/bin/R
-            tmpfile=$(mktemp)
-            '' +
-            # VSCode seems unable to properly read dev shell environment 
-            # variables => manually patch local settings
+          };
+          rWrapper = pkgs.rWrapper.override {
+            packages = rPackageList;
+          };
+        in
+        pkgs.mkShell {
+          shellHook =
             ''
-            cat .vscode/settings.json |
-              jq '."r.rterm.linux" = env.RADIAN_BIN | ."r.rpath.linux" = env.R_BIN' > \
-              $tmpfile
-            mv $tmpfile .vscode/settings.json
-            '';
+              export RADIAN_BIN=${radianWrapper.outPath}/bin/radian
+              export R_BIN=${rWrapper.outPath}/bin/R
+              tmpfile=$(mktemp)
+            ''
+            +
+              # VSCode seems unable to properly read dev shell environment
+              # variables => manually patch local settings
+              ''
+                cat .vscode/settings.json |
+                  jq '."r.rterm.linux" = env.RADIAN_BIN | ."r.rpath.linux" = env.R_BIN' > \
+                  $tmpfile
+                mv $tmpfile .vscode/settings.json
+              '';
           buildInputs = [
-              rWrapper
-              radianWrapper
-              pkgs.jq
-              ]; }
-    );
-  };
+            rWrapper
+            radianWrapper
+            pkgs.jq
+          ];
+        }
+      );
+    };
 }
