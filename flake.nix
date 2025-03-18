7890143655e79b6f4cb313107f3f335eb422f86f
Leon Di Stefano <l@sdf.org>
2025-03-18 16:58:01 +1100

    Tidy with Copilot

--- flake.nix
+++ flake.nix
@@ -3,102 +3,75 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      ...
-    }:
+  outputs = { self, nixpkgs, ... }: let
+    l = nixpkgs.lib // builtins;
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-darwin"
+    ];
+    forAllSystems = f: l.genAttrs supportedSystems (system: f system (import nixpkgs { inherit system; }));
 
-    let
-      l = nixpkgs.lib // builtins;
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-darwin"
-      ];
-      forAllSystems =
-        f: l.genAttrs supportedSystems (system: f system (import nixpkgs { inherit system; }));
+    # Define R packages used across devShell and build
+    commonRPackages = pkgs: with pkgs.rPackages; [
+      ggplot2
+      cowplot
+      colorspace
+      forcats
+      rlang
+    ];
 
-    in
+    vscodeRPackages = pkgs: with pkgs.rPackages; [
+      languageserver
+      httpgd
+      jsonlite
+      devtools
+    ];
 
-    {
-      # enter this python environment by executing `nix shell .`
-      devShell = forAllSystems (
-        system: pkgs:
-        let
-          rPackages = pkgs.rPackages;
+    additionalRPackages = pkgs: with pkgs.rPackages; [
+      eulerr
+      qvalue
+      tidyverse
+    ];
 
-          vscodeRPackages = with rPackages; [
-            languageserver
-            httpgd
-            rlang
-            jsonlite
-            # needed for knitr I think
-            devtools
-          ];
-          additionalRPackages = with rPackages; [
-            # Required by the package itself
-            ggplot2
-            cowplot
-            colorspace
-            # patchwork
-            forcats
+    rPackageList = pkgs: vscodeRPackages pkgs ++ additionalRPackages pkgs ++ commonRPackages pkgs;
 
-            # For building the readme
-            eulerr
-            qvalue
-
-            # For development
-            tidyverse
-          ];
-          rPackageList = vscodeRPackages ++ additionalRPackages;
-          radianWrapper = pkgs.radianWrapper.override {
-            packages = rPackageList;
-          };
-          rWrapper = pkgs.rWrapper.override {
-            packages = rPackageList;
-          };
-        in
-        pkgs.mkShell {
-          shellHook =
-            ''
-              export RADIAN_BIN=${radianWrapper.outPath}/bin/radian
-              export R_BIN=${rWrapper.outPath}/bin/R
-              tmpfile=$(mktemp)
-            ''
-            +
-              # VSCode seems unable to properly read dev shell environment
-              # variables => manually patch local settings
-              ''
-                cat .vscode/settings.json |
-                  jq '."r.rterm.linux" = env.RADIAN_BIN | ."r.rpath.linux" = env.R_BIN' > \
-                  $tmpfile
-                mv $tmpfile .vscode/settings.json
-              '';
-          buildInputs = [
-            rWrapper
-            radianWrapper
-            pkgs.jq
-          ];
-        }
-      );
-      packages = forAllSystems (
-        # Required by the package itself
-        system: pkgs: {
-          default = pkgs.rPackages.buildRPackage {
-            name = "rankGap";
-            src = ./.;
-            propagatedBuildInputs = with pkgs.rPackages; [
-              colorspace
-              cowplot
-              forcats
-              ggplot2
-              rlang
-              scales
-              tibble
-            ];
-          };
-        }
-      );
+    # Define R wrappers
+    radianWrapper = pkgs: pkgs.radianWrapper.override {
+      packages = rPackageList pkgs;
     };
+
+    rWrapper = pkgs: pkgs.rWrapper.override {
+      packages = rPackageList pkgs;
+    };
+
+  in {
+    # Development shell
+    devShell = forAllSystems (system: pkgs: pkgs.mkShell {
+      shellHook = ''
+        export RADIAN_BIN=${radianWrapper pkgs}/bin/radian
+        export R_BIN=${rWrapper pkgs}/bin/R
+        tmpfile=$(mktemp)
+
+        # VSCode seems unable to properly read dev shell environment variables
+        # => manually patch local settings
+        cat .vscode/settings.json |
+          jq '."r.rterm.linux" = env.RADIAN_BIN | ."r.rpath.linux" = env.R_BIN' > $tmpfile
+        mv $tmpfile .vscode/settings.json
+      '';
+      buildInputs = [
+        (rWrapper pkgs)
+        (radianWrapper pkgs)
+        pkgs.jq
+      ];
+    });
+
+    # Package definition
+    packages = forAllSystems (system: pkgs: {
+      default = pkgs.rPackages.buildRPackage {
+        name = "rankGap";
+        src = ./.;
+        propagatedBuildInputs = commonRPackages pkgs;
+      };
+    });
+  };
 }
